25861e8e7b98dc78d92f0882bcac957fa3e227df
Kristian Mandrup <kmandrup@gmail.com>
2018-04-08 21:02:46 +0100

    while tests pass :)

--- __tests__/ts/node/tester/statements/loops/while-loop.test.ts
+++ __tests__/ts/node/tester/statements/loops/while-loop.test.ts
@@ -8,16 +8,26 @@ describe('while loop', () => {
   describe('while do', () => {
     context('while-do file', () => {
       const tester = testerFor({
-        fileName: 'while-do',
-        type: 'whileDo'
+        fileName: 'while/while-do',
+        type: 'statements/loops',
+        factoryName: 'loop.while',
+        statementIndex: 0
       })
 
       describe('info()', () => {
         it('collects correct info', () => {
           const info = tester.info()
-          logObj(info)
-          expect(info).toEqual({
-          })
+          const expected = {
+            "nestedLevels": 0,
+            "loop": true,
+            "loopType": "while",
+            "whileType": "whileDo",
+            "whileThen": true,
+            "doWhile": false
+          }
+
+          logObj('info', info)
+          expect(info).toEqual(expected)
         })
       })
 
@@ -27,16 +37,25 @@ describe('while loop', () => {
   describe('do while', () => {
     context('do-while file', () => {
       const tester = testerFor({
-        fileName: 'do-while',
-        type: 'doWhile'
+        fileName: 'while/do-while',
+        type: 'statements/loops',
+        factoryName: 'loop.while',
+        statementIndex: 0
       })
 
       describe('info()', () => {
         it('collects correct info', () => {
           const info = tester.info()
-          logObj(info)
-          expect(info).toEqual({
-          })
+          const expected = {
+            "nestedLevels": 0,
+            "loop": true,
+            "loopType": "while",
+            "whileType": "doWhile",
+            "whileThen": false,
+            "doWhile": true
+          }
+          logObj('info', info)
+          expect(info).toEqual(expected)
         })
       })
     })
