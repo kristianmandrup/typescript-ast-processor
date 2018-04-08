d22bcecd2a96c086aad2578b770ddbaafbe76e39
Kristian Mandrup <kmandrup@gmail.com>
2018-04-08 20:46:19 +0100

    ternary test passes

--- __tests__/ts/node/tester/statements/conditional/switch.test.ts
+++ __tests__/ts/node/tester/statements/conditional/switch.test.ts
@@ -8,13 +8,15 @@ describe('switch', () => {
   context('switch file', () => {
     const tester = testerFor({
       fileName: 'switch',
-      type: 'switch'
+      type: 'statements/conditional',
+      factoryName: 'condition.switch',
+      statementIndex: 1
     })
 
     describe('info()', () => {
       it('collects correct info', () => {
         const info = tester.info()
-        logObj(info)
+        logObj('info', info)
         expect(info).toEqual({
         })
       })
--- __tests__/ts/node/tester/statements/conditional/ternary.test.ts
+++ __tests__/ts/node/tester/statements/conditional/ternary.test.ts
@@ -8,14 +8,17 @@ describe('ternary: condition ? whenTrue : whenFalse', () => {
   context('ternary file', () => {
     const tester = testerFor({
       fileName: 'ternary',
-      type: 'ternary'
+      type: 'statements/conditional',
+      factoryName: 'condition.ternary',
+      statementIndex: 0
     })
 
     describe('info()', () => {
       it('collects correct info', () => {
         const info = tester.info()
-        logObj(info)
+        logObj('info', info)
         expect(info).toEqual({
+          conditionalType: "ternary"
         })
       })
     })
