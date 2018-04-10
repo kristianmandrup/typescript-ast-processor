0fd7536649c503c6ed51b750826a3ecc7f64fc09
Kristian Mandrup <kmandrup@gmail.com>
2018-04-10 21:03:49 +0100

    testing traversers WIP

--- __tests__/ts/traverser/count-traverser.test.ts
+++ __tests__/ts/traverser/count-traverser.test.ts
@@ -1,8 +1,4 @@
 import {
-  node,
-  createSrcFile,
-  fixturesPath,
-  fixtureFile,
   context,
   loadAstNode,
   traverser
@@ -14,6 +10,7 @@ describe('traverser: count-traverser', () => {
     const astNode: any = loadAstNode(filePath)
     const countTraverser = traverser.createCountingASTNodeTraverser(astNode)
 
+    // init(options)
     describe('init(options)', () => {
       context('no options', () => {
         const options = {}
@@ -28,24 +25,78 @@ describe('traverser: count-traverser', () => {
         })
       })
     })
+
+    // parseQuery(query)
+    describe('parseQuery(query)', () => {
+      context('empty query', () => {
+        let ctx: any = {}
+
+        beforeEach(() => {
+          ctx.nodeTypes = new Object(countTraverser['nodeTypes'])
+          countTraverser.parseQuery({})
+        })
+
+        it('nodeTypes unchanged', () => {
+          expect(countTraverser['nodeTypes']).toBe(ctx.nodeTypes)
+        })
+      })
+
+      context('valid query', () => {
+        let ctx: any = {}
+
+        beforeEach(() => {
+          ctx.nodeTypes = new Object(countTraverser['nodeTypes'])
+          countTraverser.parseQuery({})
+        })
+
+        it('updates nodeTypes', () => {
+          expect(countTraverser['nodeTypes']).not.toBe(ctx.nodeTypes)
+        })
+      })
+    })
+
+
+    // resolveTypeCategories
+    describe('resolveTypeCategories', () => {
+      context('can be resolved', () => {
+        it('nodeTypes unchanged', () => {
+          const result = countTraverser['resolveTypeCategories']()
+          expect(result).toEqual({})
+        })
+      })
+    })
+
+    // resolveCategoryKey(key)
+    describe('resolveCategoryKey(key)', () => {
+      context('key can be resolved', () => {
+        it('resolves key', () => {
+          const key = 'x'
+          const result = countTraverser['resolveCategoryKey'](key)
+          expect(result).toEqual({})
+        })
+      })
+
+      context('key can NOT be resolved', () => {
+        it('throws?', () => {
+          const key = 'x'
+          const result = countTraverser['resolveCategoryKey'](key)
+          expect(result).toEqual({})
+        })
+      })
+    })
+
+    // resolveTypeCategory(categoryName)
+    // counterKeyFor(key)
+    // inc(key: string, counter?: any)
+    // skipped(node: any)
+    // resolveNodeTypesToCheckFor
+    // nodeTypesToCheckFor
+    // typeOf(node)
+    // checkIfNodeToBeCounted(node)
+    // shouldCountNode(node)
+    // countVisitedNodeType(node)
+    // wasVisited(node)
+    // shouldExcludeNodeFromVisit(node)
+    // shouldVisitNode(node)
   })
 })
-
-// init(options)
-// parseQuery(query)
-// resolveTypeCategories
-// resolveCategoryKey(key)
-// resolveTypeCategory(categoryName)
-// counterKeyFor(key)
-// inc(key: string, counter?: any)
-// skipped(node: any)
-// resolveNodeTypesToCheckFor
-// nodeTypesToCheckFor
-// typeOf(node)
-// checkIfNodeToBeCounted(node)
-// shouldCountNode(node)
-// countVisitedNodeType(node)
-// wasVisited(node)
-// shouldExcludeNodeFromVisit(node)
-// shouldVisitNode(node)
-
--- __tests__/ts/traverser/traverser.test.ts
+++ __tests__/ts/traverser/traverser.test.ts
@@ -1,21 +1,83 @@
-// init(options)
-// registerVisitors(registry)
-// registerVisitor(name: string, visitor: Function)
-// visitorIterator
-// nodeDisplayInfo(node)
-// typeOf(node)
-// skipped(node)
-// typeNameFor(typeName: string, fnName?: string)
-// wasVisited(node: any)
-// willVisit(node: any)
-// wasVisitedBefore(node: any)
-// visit(nextNode?: ts.Node)
-// shouldTraverseChildNodes(node: ts.Node)
-// shouldTraverseAncestor(node: ts.Node)
-// shouldExcludeNodeFromVisit(node: any)
-// shouldVisitNode(node: any)
-// traverseNextMethod
-// traverseNext(node: any)
-// traverseChildNodes(node: ts.Node)
-// traverseAncestor(node: ts.Node)
+import {
+  context,
+  loadAstNode,
+  traverser
+} from './_imports'
 
+describe('traverser: count-traverser', () => {
+  context('count-traverser file', () => {
+    const filePath = 'traverser/count-traverser'
+    const astNode: any = loadAstNode(filePath)
+    const $traverser = traverser.createCountingASTNodeTraverser(astNode)
+
+    // init(options)
+    describe('init(options)', () => {
+      context('no options', () => {
+        const options = {}
+        beforeEach(() => {
+          $traverser.init(options)
+        })
+
+        it('initializes traverser with categories of nodeTypes', () => {
+          expect($traverser['nodeTypes'].categories).toEqual({
+
+          })
+        })
+      })
+    })
+
+    // registerVisitors(registry)
+    describe('registerVisitors(registry)', () => {
+      context('valid registry', () => {
+        const registries = {
+          valid: {
+
+          },
+          invalid: {
+
+          }
+        }
+        it('initializes traverser with categories of nodeTypes', () => {
+          expect($traverser.registerVisitors(registries.valid)).toEqual({
+          })
+        })
+      })
+
+      context('invalid registry', () => {
+        const registries = {
+          valid: {
+
+          },
+          invalid: {
+
+          }
+        }
+
+        it('initializes traverser with categories of nodeTypes', () => {
+          expect($traverser.registerVisitors(registries.valid)).toEqual({
+          })
+        })
+      })
+    })
+
+
+    // registerVisitor(name: string, visitor: Function)
+    // visitorIterator
+    // nodeDisplayInfo(node)
+    // typeOf(node)
+    // skipped(node)
+    // typeNameFor(typeName: string, fnName?: string)
+    // wasVisited(node: any)
+    // willVisit(node: any)
+    // wasVisitedBefore(node: any)
+    // visit(nextNode?: ts.Node)
+    // shouldTraverseChildNodes(node: ts.Node)
+    // shouldTraverseAncestor(node: ts.Node)
+    // shouldExcludeNodeFromVisit(node: any)
+    // shouldVisitNode(node: any)
+    // traverseNextMethod
+    // traverseNext(node: any)
+    // traverseChildNodes(node: ts.Node)
+    // traverseAncestor(node: ts.Node)
+  })
+})
