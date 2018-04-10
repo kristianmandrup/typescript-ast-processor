ec98e67fb76eea0faa2673af0841bd890a0dda41
Kristian Mandrup <kmandrup@gmail.com>
2018-04-10 15:51:01 +0100

    better getter test

--- __tests__/ts/node/tester/declarations/class/members/getter.test.ts
+++ __tests__/ts/node/tester/declarations/class/members/getter.test.ts
@@ -1,6 +1,5 @@
 import {
   testerFor,
-  query,
   context,
   logObj
 } from '../_imports'
@@ -21,15 +20,58 @@ describe('class', () => {
       })
 
       const query = {
-        noMatch: {
-          anyOf: ['unknown']
+        binary: {
+          notMatch: {
+            not: {
+              name: 'unknown'
+            }
+          }
         },
-        anyOf: {
-          anyOf: ['name']
+        matches: {
+          match: {
+            name: 'name',
+            access: {
+              exactly: 'protected',
+            }
+          },
+          noMatch: {
+            name: 'unknown',
+          }
         },
-        allOf: {
-          allOf: ['name']
+        access: {
+          exactly: 'protected',
+          anyOf: [
+            'private', 'public'
+          ],
+          notAny: [
+            'public'
+          ]
         },
+        name: {
+          hasAny: {
+            anyOf: ['name']
+          },
+          notAny: {
+            anyOf: ['unknown']
+          },
+          hasAll: {
+            allOf: ['name']
+          }
+        },
+        parameters: {
+          hasAny: {
+            anyOf: ['v', 'x']
+          },
+          notAny: {
+            anyOf: ['a', 'b']
+          },
+          notAll: {
+            allOf: ['v', 'x']
+          },
+          haveAll: {
+            allOf: ['v']
+          }
+        }
       }
 
 
@@ -53,17 +95,45 @@ describe('class', () => {
       describe.skip('not', () => {
         describe('testAccess(query)', () => {
           it('not anyOf: A - true', () => {
-            const result = tester.testAccess(query.anyOf)
+            const result = tester.testAccess(query.binary.notMatch)
             expect(result).toBe(true)
           })
         })
       })
 
+      describe('testName(query)', () => {
+        it('name: notAny: false', () => {
+          const res = tester.testName(query.name.notAny)
+          expect(res).toBe(false)
+        })
+
+        it('name: hasAny: true', () => {
+          const res = tester.testName(query.name.hasAny)
+          expect(res).toBe(true)
+        })
+
+        it('name: hasAll: true', () => {
+          const res = tester.testName(query.name.hasAll)
+          expect(res).toBe(true)
+        })
+      })
+
+      describe('testParameters(query)', () => {
+        it('parameters: notAny: false', () => {
+          const res = tester.testParameters(query.parameters.notAny)
+          expect(res).toBe(false)
+        })
+
+        it('parameters: hasAny: false', () => {
+          const res = tester.testParameters(query.parameters.hasAny)
+          expect(res).toBe(true)
+        })
+      })
 
       describe.skip('testAccess(query)', () => {
         context('has getter and setter for name', () => {
           it('anyOf: name - true ', () => {
-            const res = tester.testAccess(query.anyOf)
+            const res = tester.testAccess(query.access.anyOf)
             log('should match', { res })
             expect(res).not.toBe(false)
             expect(res.result).toBe(true)
@@ -72,24 +142,24 @@ describe('class', () => {
 
         context('has no matching accessors for unknown', () => {
           it('anyOf: name - true ', () => {
-            const res = tester.testAccess(query.noMatch)
+            const res = tester.testAccess(query.access)
             log('no match', { res })
             expect(res).toBe(false)
           })
         })
       })
 
-      describe.skip('query(query)', () => {
+      describe('query(query)', () => {
         it('members: anyOf: Ix, Iy - false', () => {
-          const res = tester.query(query.anyOf)
+          const res = tester.query(query.matches.match)
           expect(res.implements).toEqual(['Ix'])
           expect(res.result).toBe(true)
         })
       })
 
-      describe.skip('test(query)', () => {
+      describe('test(query)', () => {
         it('members: anyOf: Ix, Iy - false', () => {
-          const res = tester.test(query.anyOf)
+          const res = tester.test(query.matches.noMatch)
           expect(res).toBe(true)
         })
       })
